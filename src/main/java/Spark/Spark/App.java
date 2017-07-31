bba7cf2482a944ff9b69395db15b9946b9cf65b6
tomfans <30590693+tomfans@users.noreply.github.com>
2017-07-31 17:13:49 +0800

    Initial commit

---
+++ src/main/java/Spark/Spark/App.java
@@ -0,0 +1,13 @@
+package Spark.Spark;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
